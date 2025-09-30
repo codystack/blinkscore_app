59e8a34f1aec2cb900d82efd4161bffed8ee359b
codystack <ebuzzadvert@gmail.com>
2025-09-30 23:37:49 +0100

    404 and 505 error page added

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" class="js">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="www.webify.com.ng">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./assets/images/favicon_light.png">
+
+    <title>Error 404 :: BlinksCore&trade;</title>
+    
+    <link rel="stylesheet" href="assets/css/vendor.bundle.css">
+    <link rel="stylesheet" href="assets/css/style.css" id="layoutstyle">
+</head>
+
+<body class="page-ath">
+    <div class="vh100 d-flex align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-7 col-xl-6 text-center">
+                    <div class="error-content">
+                        <span class="error-text-large">404</span>
+                        <h4 class="text-dark">Opps! Why you’re here?</h4>
+                        <p>We are very sorry for inconvenience. It looks like you’re try to access a page that either has been deleted or never existed.</p>
+                        <a href="./" class="btn btn-primary">Back to Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="assets/js/jquery.bundle.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
---
+++ 505.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" class="js">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="www.webify.com.ng">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./assets/images/favicon_light.png">
+
+    <title>Error 505 :: BlinksCore&trade;</title>
+    
+    <link rel="stylesheet" href="assets/css/vendor.bundle.css">
+    <link rel="stylesheet" href="assets/css/style.css" id="layoutstyle">
+</head>
+
+<body class="page-ath">
+    <div class="vh100 d-flex align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-7 col-xl-6 text-center">
+                    <div class="error-content">
+                        <span class="error-text-large">505</span>
+                        <h4 class="text-dark">Oops! Something went wrong on our end.</h4>
+                        <p class="mb-0 pb-0">We’re very sorry for the inconvenience. It looks like the server is unable to handle the HTTP version of your request.</p>
+                        <p class="mt-0 pt-0">Please try again later or <a href="https://blinkscore.ng/contact">contact support</a> if the issue persists.</p>
+                        <a href="./" class="btn btn-primary">Back to Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="assets/js/jquery.bundle.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
